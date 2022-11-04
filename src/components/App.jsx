3dd53ef1d656118944ffa58d64b9f840968d2949
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-11-04 10:03:08 +0200

    update app with route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+import { Container } from 'components/AppStyled';
+import  SharedLayout  from 'components/SharedLayout/SharedLayout';
+
+const Home = lazy(() => import('pages/Home/Home'));
+const Movies = lazy(() => import('pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Reviews = lazy(() => import('components/Reiwers/Reiwers'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="reiwers" element={<Reviews />} />
+            <Route path="cast" element={<Cast />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Container>
   );
 };
